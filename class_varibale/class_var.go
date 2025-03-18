cba38c9f2a9d4a369aa7493e951efcf1ad4de082
Ali Hassan <alihassanftt@gmail.com>
2025-03-18 07:15:19 +0500

    discuss define varaible method

--- class_varibale/class_var.go
+++ class_varibale/class_var.go
@@ -2,9 +2,31 @@ package main
 
 import "fmt"
 
+// 30:56minit end
+
 func main() {
-	var a int = 10
-	var b int
-	fmt.Println("variable a:", a)
-	fmt.Println("print zero", b)
+	// two mtehod declar varibale
+
+	// 1st method
+	var a int //declaration
+	a = 11    //initialization
+
+	fmt.Println("a value:", a)
+
+	// 2nd method
+	var b int = 13 //declaration and initialization
+	fmt.Println("b value: ", b)
+
+	//3rd we can direct give value to variable withoiut define type
+	var c = 21 //declaration and initialization
+	println("c value", c)
+
+	// 4th method without using keyword var and define data type
+	d := 27
+	println("d value", d)
+
+	// var a int = 10
+	// var b int
+	// fmt.Println("variable a:", a)
+	// fmt.Println("print zero", b)
 }
